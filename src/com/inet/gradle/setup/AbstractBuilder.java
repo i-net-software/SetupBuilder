63d9b8c14cb5af2612719f91aa0a01fd3edfc447
Volker Berlin <vberlin@inetsoftware.de>
2015-06-26 14:40:37 +0200

    Add AbstractBuilder

---
+++ src/com/inet/gradle/setup/AbstractBuilder.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2015 i-net software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.inet.gradle.setup;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.gradle.api.internal.file.FileResolver;
+import org.gradle.process.internal.DefaultExecAction;
+
+/**
+ * Some basic builder functionally.
+ * 
+ * @author Volker Berlin
+ *
+ * @param <T> the task
+ */
+public abstract class AbstractBuilder<T extends AbstractSetupTask> {
+
+    protected T            task;
+
+    protected FileResolver fileResolver;
+
+    protected SetupBuilder setup;
+
+    protected File         buildDir;
+
+    protected AbstractBuilder( T task, SetupBuilder setup, FileResolver fileResolver ) {
+        this.task = task;
+        this.setup = setup;
+        this.fileResolver = fileResolver;
+        this.buildDir = task.getTemporaryDir();
+    }
+
+    /**
+     * Call a program from the WIX installation.
+     * 
+     * @param tool the program name
+     * @param parameters the parameters
+     */
+    protected void exec( ArrayList<String> parameters ) {
+        // print command line to the log
+        StringBuilder log = new StringBuilder( "\t" );
+        for( String para : parameters ) {
+            log.append( '\"' ).append( para );
+            if( para.endsWith( "\\" ) ) {
+                log.append( '\\' );
+            }
+            log.append( "\" " );
+        }
+        task.getProject().getLogger().lifecycle( log.toString() );
+
+        DefaultExecAction action = new DefaultExecAction( fileResolver );
+        action.setCommandLine( parameters );
+        action.setWorkingDir( buildDir );
+        action.execute();
+    }
+}
--- src/com/inet/gradle/setup/dmg/DmgBuilder.java
+++ src/com/inet/gradle/setup/dmg/DmgBuilder.java
@@ -16,10 +16,14 @@
 package com.inet.gradle.setup.dmg;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 import org.gradle.api.Project;
 import org.gradle.api.internal.file.FileResolver;
+import org.gradle.process.internal.DefaultExecAction;
 
+import com.inet.gradle.setup.AbstractBuilder;
 import com.inet.gradle.setup.DocumentType;
 import com.inet.gradle.setup.SetupBuilder;
 import com.inet.gradle.setup.image.ImageFactory;
@@ -32,15 +36,7 @@ import com.oracle.appbundler.BundleDocument;
  * 
  * @author Volker Berlin
  */
-public class DmgBuilder {
-
-    private final Dmg          dmg;
-
-    private final SetupBuilder setup;
-
-    private FileResolver       fileResolver;
-
-    private File               buildDir;
+public class DmgBuilder extends AbstractBuilder<Dmg> {
 
     /**
      * Create a new instance
@@ -50,15 +46,12 @@ public class DmgBuilder {
      * @param fileResolver the file Resolver
      */
     public DmgBuilder( Dmg dmg, SetupBuilder setup, FileResolver fileResolver ) {
-        this.dmg = dmg;
-        this.setup = setup;
-        this.fileResolver = fileResolver;
+        super( dmg, setup, fileResolver );
     }
 
     public void build() {
         try {
-            Project project = dmg.getProject();
-            buildDir = dmg.getTemporaryDir();
+            Project project = task.getProject();
 
             String name = setup.getBaseName();
             AppBundlerTask appBundler = new AppBundlerTask();
@@ -108,7 +101,10 @@ public class DmgBuilder {
 
             appBundler.execute();
 
-            dmg.copyTo( new File( buildDir, name + ".app/Contents/Java" ) );
+            task.copyTo( new File( buildDir, name + ".app/Contents/Java" ) );
+            Files.copy( iconFile.toPath(), new File( buildDir, ".VolumeIcon.icns" ).toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING );
+
+            createBinary();
         } catch( RuntimeException ex ) {
             throw ex;
         } catch( Exception ex ) {
@@ -116,4 +112,16 @@ public class DmgBuilder {
         }
     }
 
+    private void createBinary() {
+        setFile();
+    }
+
+    private void setFile() {
+        ArrayList<String> command = new ArrayList<>();
+        command.add( "SetFile" );
+        command.add( "-c" );
+        command.add( "icnC" );
+        command.add( buildDir + "/.VolumeIcon.icns" );
+        exec( command );
+    }
 }
