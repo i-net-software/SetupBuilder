31536263c7141fb02dd662f3a99812bc8a955f9e
Gerry Weißbach <gerry.w@gammaproduction.de>
2015-07-21 08:10:25 +0200

    Recursively remove temporary files.

--- src/com/inet/gradle/setup/dmg/DmgBuilder.java
+++ src/com/inet/gradle/setup/dmg/DmgBuilder.java
@@ -23,8 +23,11 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileAttribute;
 import java.util.ArrayList;
 
@@ -135,9 +138,23 @@ public class DmgBuilder extends AbstractBuilder<Dmg> {
             task.copyTo( new File( buildDir, applicationName + ".app/Contents/Java" ) );
             
             createBinary();
+
+            // Remove temporary folder and content.
+            Files.walkFileTree(tmp, new SimpleFileVisitor<Path>() {
+         	   @Override
+         	   public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+         		   Files.delete(file);
+         		   return FileVisitResult.CONTINUE;
+         	   }
+
+         	   @Override
+         	   public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+         		   Files.delete(dir);
+         		   return FileVisitResult.CONTINUE;
+         	   }
+
+            });
             
-            // Remove temporary Path 
-            Files.delete(tmp);
             tmp = null;
         
         } catch( RuntimeException ex ) {
