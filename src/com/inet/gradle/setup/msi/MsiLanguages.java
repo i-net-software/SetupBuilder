5a9d1eb5d88b231de51cdb12ac78898553f63174
Volker Berlin <vberlin@inetsoftware.de>
2015-08-06 13:48:51 +0200

    Add more languages ids

--- src/com/inet/gradle/setup/msi/MsiLanguages.java
+++ src/com/inet/gradle/setup/msi/MsiLanguages.java
@@ -21,12 +21,48 @@ package com.inet.gradle.setup.msi;
  * @author Volker Berlin
  */
 enum MsiLanguages {
+    // Available Language Packs for Windows: https://technet.microsoft.com/en-us/library/hh825678.aspx
+    // from WixUIExtension supported languages: https://github.com/wixtoolset/wix3/tree/develop/src/ext/UIExtension/wixlib
+
+    en_us( 1033 ), // this should be the default, that it is on position 0
+
+    ar_sa( 1025 ), //
+    bg_bg( 1026 ), //
+    ca_es( 1027 ), //
     zh_tw( 1028 ), //
+    cs_cz( 1029 ), //
+    da_dk( 1030 ), //
     de_de( 1031 ), //
-    en_us( 1033 ), //
+    el_gr( 1032 ), //
+    fi_fi( 1035 ), //
     fr_fr( 1036 ), //
+    he_IL( 1037 ), //
+    hu_hu( 1038 ), //
+    it_it( 1040 ), //
     ja_jp( 1041 ), //
+    ko_kr( 1042 ), //
+    nl_nl( 1043 ), //
+    nb_no( 1044 ), //
+    pl_pl( 1045 ), //
+    pt_br( 1046 ), //
+    ro_ro( 1048 ), //
+    ru_ru( 1049 ), //
+    hr_hr( 1050 ), //
+    sk_sk( 1051 ), //
+    sv_se( 1053 ), //
+    th_th( 1054 ), //
+    tr_tr( 1055 ), //
+    uk_ua( 1058 ), //
+    sl_si( 1060 ), //
+    et_ee( 1061 ), //
+    lv_lv( 1062 ), //
+    lt_lt( 1063 ), //
+//    hi_in( 1081 ), // error LGHT0311 : A string was provided with characters that are not available in the specified database code page '1252'. Either change these characters to ones that exist in the database's code page, or update the database's code page by modifying one of the following attributes: Product/@Codepage, Module/@Codepage, Patch/@Codepage, PatchCreation/@Codepage, or WixLocalization/@Codepage.
+//    kk_kz( 1087 ), // error LGHT0311 : A string was provided with characters that are not available in the specified database code page '1252'. Either change these characters to ones that exist in the database's code page, or update the database's code page by modifying one of the following attributes: Product/@Codepage, Module/@Codepage, Patch/@Codepage, PatchCreation/@Codepage, or WixLocalization/@Codepage.
     zh_cn( 2052 ), //
+    pt_pt( 2070 ), //
+    sr_latn_cs( 2074 ), //
+    zh_hk( 3076 ), //
     es_es( 3082 ), //
     ;
 
