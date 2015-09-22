8b4fadf3abba61cb3cfebf9466b997ca6955399d
Stefan Heidrich <sheidrich@arcor.de>
2015-09-22 14:54:10 +0200

    added API Doc for die Getter and Setter

--- src/com/inet/gradle/setup/rpm/Rpm.java
+++ src/com/inet/gradle/setup/rpm/Rpm.java
@@ -29,23 +29,11 @@ import com.inet.gradle.setup.AbstractSetupTask;
  */
 public class Rpm extends AbstractSetupTask {
 	
-    private String                 section;
+    private String                  section;
 
-//    private String                 priority;
-
-    private String                 architecture;
-
-//    private String                 installSize;
-    
-//    private String                 recommends;
+    private String                  architecture;
     
     private String                 	depends;
-    
-    private String                 	homepage;
-    
-    private String				   	maintainerEmail;
-    
-    private String				   	changes;
     
     private String				   	summary;
     
@@ -73,6 +61,9 @@ public class Rpm extends AbstractSetupTask {
 
 	
 
+	/**
+	 * the default constructor
+	 */
     public Rpm() {
         super( "rpm" );
     }
@@ -94,188 +85,242 @@ public class Rpm extends AbstractSetupTask {
     	super.copyTo(target);
     }
       
-
+    /**
+     * Returns the section that specifies the 'Group' entry in the SPEC file.
+     * @return the section
+     */
 	public String getSection() {
 		return section;
 	}
 
-
+	/**
+	 * Sets the value for the 'Group' entry in the SPEC file.
+	 * @param section the value for the entry
+	 */
 	public void setSection(String section) {
 		this.section = section;
 	}
 
-
-//	public String getPriority() {
-//		return priority;
-//	}
-//
-//
-//	public void setPriority(String priority) {
-//		this.priority = priority;
-//	}
-
-
+	/**
+     * Returns the architecture that specifies the 'BuildArchitectures' entry in the SPEC file.
+     * @return the architecture specified in the gradle script
+     */
 	public String getArchitecture() {
 		return architecture;
 	}
 
-
+	/**
+	 * Sets the value for the 'BuildArchitectures' entry in the SPEC file.
+	 * @param architecture the value for the entry
+	 */
 	public void setArchitecture(String architecture) {
 		this.architecture = architecture;
 	}
 
-
-//	public String getInstallSize() {
-//		return installSize;
-//	}
-//
-//
-//	public void setInstallSize(String installSize) {
-//		this.installSize = installSize;
-//	}
-//
-//
-//	public String getRecommends() {
-//		return recommends;
-//	}
-//
-//
-//	public void setRecommends(String recommends) {
-//		this.recommends = recommends;
-//	}
-//
-//
+	/**
+	 * Returns the dependency that should be used in the 'Requires' entry in the SPEC file.
+	 * @return the dependency specified in the gradle script
+	 */
 	public String getDepends() {
 		return depends;
 	}
 
-
+	/**
+	 * Sets the value for the 'Requires' entry in the SPEC file.
+	 * @param depends the value for the entry
+	 */
 	public void setDepends(String depends) {
 		this.depends = depends;
 	}
 
-
-	public String getHomepage() {
-		return homepage;
-	}
-
-
-	public void setHomepage(String homepage) {
-		this.homepage = homepage;
-	}
-
-
-	public String getMaintainerEmail() {
-		return maintainerEmail;
-	}
-
-
-	public void setMaintainerEmail(String maintainerEmail) {
-		this.maintainerEmail = maintainerEmail;
-	}
-
-
-	public String getChanges() {
-		return changes;
-	}
-
-
-	public void setChanges(String changes) {
-		this.changes = changes;
-	}
-
+	/**
+	 * Returns the summary that should be used in the 'Summary' entry in the SPEC file.
+	 * @return the summary specified in the gradle script
+	 */
 	public String getSummary() {
 		return summary;
 	}
-
+	
+	/**
+	 * Sets the value for the 'Summary' entry in the SPEC file.
+	 * @param summary the value for the entry
+	 */
 	public void setSummary(String summary) {
 		this.summary = summary;
 	}
 
+	/**
+	 * Returns the release that should be used in the 'Release' entry in the SPEC file.
+	 * @return the release specified in the gradle script
+	 */
 	public String getRelease() {
 		return release;
 	}
 
+	/**
+	 * Sets the value for the 'Release' entry in the SPEC file.
+	 * @param release the value for the entry
+	 */
 	public void setRelease(String release) {
 		this.release = release;
 	}
 
+	/**
+	 * Returns the license that should be used in the 'License' entry in the SPEC file.
+	 * @return the license specified in the gradle script
+	 */
 	public String getLicense() {
 		return license;
 	}
 
+	/**
+	 * Sets the value for the 'License' entry in the SPEC file.
+	 * @param license the value for the entry
+	 */
 	public void setLicense(String license) {
 		this.license = license;
 	}
 
+	/**
+	 * Returns the url that should be used in the 'URL' entry in the SPEC file.
+	 * @return the url specified in the gradle script
+	 */
 	public String getUrl() {
 		return url;
 	}
 
+	/**
+	 * Sets the value for the 'URL' entry in the SPEC file.
+	 * @param url the value for the entry
+	 */
 	public void setUrl(String url) {
 		this.url = url;
 	}
 
+	/**
+	 * Returns the prep that should be used in the '%prep' entry in the SPEC file.
+	 * @return the prep specified in the gradle script
+	 */
 	public ArrayList<String> getPrep() {
 		return prep;
 	}
 
+	/**
+	 * Sets the value for the '%prep' entry in the SPEC file.
+	 * @param prep the value for the entry
+	 */
 	public void setPrep(String prep) {
 		this.prep.add( prep );
 	}
 
+	/**
+	 * Returns the preun that should be used in the '%preun' entry in the SPEC file.
+	 * @return the preun specified in the gradle script
+	 */
 	public ArrayList<String> getPreun() {
 		return preun;
 	}
 	
+	/**
+	 * Sets the value for the '%preun' entry in the SPEC file.
+	 * @param preun the value for the entry
+	 */
 	public void setPreun(String preun) {
 		this.preun.add( preun );
 	}
 
+	/**
+	 * Returns the post that should be used in the '%post' entry in the SPEC file.
+	 * @return the post specified in the gradle script
+	 */
 	public ArrayList<String> getPost() {
 		return post;
 	}
 	
+	/**
+	 * Sets the value for the '%post' entry in the SPEC file.
+	 * @param post the value for the entry
+	 */
 	public void setPost(String post) {
 		this.post.add( post );
 	}
 	
+	/**
+	 * Returns the postun that should be used in the '%postun' entry in the SPEC file.
+	 * @return the postun specified in the gradle script
+	 */
 	public ArrayList<String> getPostun() {
 		return postun;
 	}
 	
+	/**
+	 * Sets the value for the '%postun' entry in the SPEC file.
+	 * @param postun the value for the entry
+	 */
 	public void setPostun(String post) {
 		this.postun.add( post );
 	}
 
+	/**
+	 * Returns the clean that should be used in the '%clean' entry in the SPEC file.
+	 * @return the clean specified in the gradle script
+	 */
 	public ArrayList<String> getClean() {
 		return clean;
 	}
 	
+	/**
+	 * Sets the value for the '%clean' entry in the SPEC file.
+	 * @param clean the value for the entry
+	 */
 	public void setClean(String clean) {
 		this.clean.add( clean );
 	}
 
+	/**
+	 * Returns the install that should be used in the '%install' entry in the SPEC file.
+	 * @return the install specified in the gradle script
+	 */
 	public ArrayList<String> getInstall() {
 		return install;
 	}
 	
+	/**
+	 * Sets the value for the '%install' entry in the SPEC file.
+	 * @param install the value for the entry
+	 */
 	public void setInstall(String install) {
 		this.install.add( install );
 	}
 
+	/**
+	 * Returns the build that should be used in the '%build' entry in the SPEC file.
+	 * @return the build specified in the gradle script
+	 */
 	public ArrayList<String> getBuild() {
 		return build;
 	}
 	
+	/**
+	 * Sets the value for the '%build' entry in the SPEC file.
+	 * @param build the value for the entry
+	 */
 	public void setBuild(String build) {
 		this.build.add( build );
 	}
 
+	/**
+	 * Returns the pre that should be used in the '%pre' entry in the SPEC file.
+	 * @return the pre specified in the gradle script
+	 */
 	public ArrayList<String> getPre() {
 		return pre;
 	}
 
+	/**
+	 * Sets the value for the '%pre' entry in the SPEC file.
+	 * @param pre the value for the entry
+	 */
 	public void setPre(ArrayList<String> pre) {
 		this.pre = pre;
 	}
