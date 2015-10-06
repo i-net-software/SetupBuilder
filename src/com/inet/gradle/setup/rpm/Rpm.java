6d7605a88b54401dc66ee755cb2285930bc630d2
Stefan Heidrich <sheidrich@inetsoftware.de>
2015-10-06 09:23:21 +0200

    Not always quoting the installation root, because this will result in an invalid files entry in the SPEC file

--- src/com/inet/gradle/setup/rpm/Rpm.java
+++ src/com/inet/gradle/setup/rpm/Rpm.java
@@ -341,7 +341,7 @@ public class Rpm extends AbstractSetupTask {
 	 */
 	public String getInstallationRoot() {
 		if(installationRoot == null) {
-			return "\"/usr/share/" + getSetupBuilder().getApplication() + "\"";
+			return "/usr/share/" + getSetupBuilder().getApplication();
 		} else {
 			return "\"" + installationRoot + "\"";
 		}
