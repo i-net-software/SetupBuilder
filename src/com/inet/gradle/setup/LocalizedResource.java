063d618c68c27a07e49410a1623357d8c38f94d6
Gerry Weißbach <gerry.w@gammaproduction.de>
2016-04-21 14:02:44 +0200

    Add functions to resolve a certain localized resource and to create a new localized resource from the configuration

--- src/com/inet/gradle/setup/LocalizedResource.java
+++ src/com/inet/gradle/setup/LocalizedResource.java
@@ -1,7 +1,12 @@
 package com.inet.gradle.setup;
 
 import java.io.File;
+import java.util.List;
 import java.util.Locale;
+
+import org.gradle.util.ConfigureUtil;
+
+import groovy.lang.Closure;
 
 /**
  * Stub Object for localized resources
@@ -41,6 +46,13 @@ public class LocalizedResource {
 	public Locale getLocale() {
 		return locale;
 	}
+	
+	/**
+	 * @return the language
+	 */
+	public String getLanguage() {
+		return locale.getLanguage();
+	}
 
 	/**
 	 * @param locale the locale to set
@@ -55,4 +67,41 @@ public class LocalizedResource {
 	public void setResource(Object resource) {
 		this.resource = resource;
 	}
+	
+    /**
+     * Return the localized file for a specific locale
+     * @param list from which to receive the resource
+     * @param locale for which to get the file
+     * @return license file
+     */
+    public static File getLocalizedResourceFile( List<LocalizedResource> list, String locale ) {
+    	
+    	for (LocalizedResource res : list) {
+			if ( locale.equalsIgnoreCase( res.getLanguage()) ) {
+				return res.getResource();
+			}
+		}
+    	
+        return null;
+    }
+
+
+    /**
+     * Set the license file
+     * @param parent the setup builder
+     * @param holder the list to add the entry to
+     * @param resource file file or closure
+     */
+    public static void addLocalizedResource( SetupBuilder parent, List<LocalizedResource> holder, Object resource ) {
+    	
+    	LocalizedResource res = new LocalizedResource( parent );
+    	if ( resource instanceof Closure<?> ) {
+    		res = ConfigureUtil.configure((Closure<?>)resource, res);
+    	} else {
+        	res.setLocale( parent.getDefaultResourceLanguage() );
+        	res.setResource( resource );
+    	}
+    	
+    	holder.add( res );
+    }
 }
