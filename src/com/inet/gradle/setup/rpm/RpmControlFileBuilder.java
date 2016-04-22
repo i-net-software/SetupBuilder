899dfe1281d869ab7c4e54f10933be449dcce18b
Stefan Heidrich <sheidrich@arcor.de>
2016-04-22 10:55:07 +0200

    after %description must be a NEWLINE

--- src/com/inet/gradle/setup/rpm/RpmControlFileBuilder.java
+++ src/com/inet/gradle/setup/rpm/RpmControlFileBuilder.java
@@ -489,7 +489,7 @@ class RpmControlFileBuilder {
         	
         	String lang = desc.getLanguage();
         	String content = NEWLINE + "%description";
-        	content += ( lang.equalsIgnoreCase(setup.getDefaultResourceLanguage()) ? " -l" + lang + NEWLINE : "" );
+        	content += ( lang.equalsIgnoreCase(setup.getDefaultResourceLanguage()) ? " -l" + lang + NEWLINE : "" + NEWLINE );
         	
         	try ( Scanner scanner = new Scanner( desc.getResource(), "UTF8" ) ) {
         		content += scanner.useDelimiter("\\A").next();
