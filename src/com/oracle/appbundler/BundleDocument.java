2d1fc5c81e97c1a02902e17a0f32178a79c04ff4
Gerry Weißbach <gerry.w@gammaproduction.de>
2017-08-02 12:09:13 +0200

    Update the appbundler to our most recent fork, pulling back the changes from the master repository. It now supports Java9 for non-JNLP apps. For JNLP we had to restrict the runtime to Java 8 due to internal changes of Java 9 that can not yet be reflected by the appbundler.

--- src/com/oracle/appbundler/BundleDocument.java
+++ src/com/oracle/appbundler/BundleDocument.java
@@ -24,17 +24,23 @@
 package com.oracle.appbundler;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.tools.ant.BuildException;
 
 
 /**
  * Represent a CFBundleDocument.
  */
-public class BundleDocument {
-    private String name = "editor";
-    private String role = "";
+public class BundleDocument implements IconContainer {
+    private String name = null;
+    private String role = "Editor";
     private String icon = null;
-    private String[] extensions;
+    private String handlerRank = null;
+    private List<String> extensions;
+    private List<String> contentTypes;
+    private List<String> exportableTypes;
     private boolean isPackage = false;
 
     private String capitalizeFirst(String string) {
@@ -43,15 +49,46 @@ public class BundleDocument {
         return new String(stringArray);
     }
     
-    public void setExtensions(String extensionsList) {
-        if(extensionsList == null) {
-            throw new BuildException("Extensions can't be null");
+    public void setExtensions(String extensionsString) {
+        extensions = getListFromCommaSeparatedString(extensionsString, "Extensions", true);
+    }
+
+    public void setContentTypes(String contentTypesString) {
+        contentTypes = getListFromCommaSeparatedString(contentTypesString, "Content Types");
+    }
+
+    public void setExportableTypes(String exportableTypesString) {
+        exportableTypes = getListFromCommaSeparatedString(exportableTypesString, "Exportable Types");
+    }
+
+    public static List<String> getListFromCommaSeparatedString(String listAsString,
+            final String attributeName) {
+        return getListFromCommaSeparatedString(listAsString, attributeName, false);
+    }
+            
+    public static List<String> getListFromCommaSeparatedString(String listAsString,
+            final String attributeName, final boolean lowercase) {
+        if(listAsString == null) {
+            throw new BuildException(attributeName + " can't be null");
         }
         
-        extensions = extensionsList.split(",");
-        for (String extension : extensions) {
-            extension.trim().toLowerCase();
+        String[] splittedListAsString = listAsString.split(",");
+        List<String> stringList = new ArrayList<String>();
+        
+        for (String extension : splittedListAsString) {
+            String cleanExtension = extension.trim();
+            if (lowercase) {
+                cleanExtension = cleanExtension.toLowerCase();
+            }
+            if (cleanExtension.length() > 0) {
+                stringList.add(cleanExtension);
+            }
         }
+        
+        if (stringList.size() == 0) {
+            throw new BuildException(attributeName + " list must not be empty");
+        }
+        return stringList;
     }
     
     public void setIcon(String icon) {
@@ -66,6 +103,10 @@ public class BundleDocument {
       this.role = capitalizeFirst(role);
     }
     
+    public void setHandlerRank(String handlerRank) {
+      this.handlerRank = capitalizeFirst(handlerRank);
+    } 
+      
     public void setIsPackage(String isPackageString) {
         if(isPackageString.trim().equalsIgnoreCase("true")) {
             this.isPackage = true;
@@ -85,9 +126,31 @@ public class BundleDocument {
     public String getRole() {
         return role;
     }
+
+    public String getHandlerRank() {
+        return handlerRank;
+    }
     
-    public String[] getExtensions() {
+    public List<String> getExtensions() {
         return extensions;
+    }
+    
+    public List<String> getContentTypes() {
+        return contentTypes;
+    }
+    
+    public List<String> getExportableTypes() {
+        return exportableTypes;
+    }
+    
+    public File getIconFile() {
+        if (icon == null) { return null; }
+
+        File ifile = new File (icon);
+        
+        if (! ifile.exists ( ) || ifile.isDirectory ( )) { return null; }
+
+        return ifile;
     }
     
     public boolean hasIcon() {
@@ -101,9 +164,24 @@ public class BundleDocument {
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder(getName());
-        s.append(" ").append(getRole()).append(" ").append(getIcon()). append(" ");
-        for(String extension : extensions) {
-            s.append(extension).append(" ");
+        s.append(" ").append(getRole())
+        .append(" ").append(getIcon())
+        .append(" ").append(getHandlerRank())
+        .append(" ");
+        if (contentTypes != null) {
+            for(String contentType : contentTypes) {
+                s.append(contentType).append(" ");
+            }
+        }
+        if (extensions != null) {
+            for(String extension : extensions) {
+                s.append(extension).append(" ");
+            }
+        }
+        if (exportableTypes != null) {
+            for(String exportableType : exportableTypes) {
+                s.append(exportableType).append(" ");
+            }
         }
         
         return s.toString();
