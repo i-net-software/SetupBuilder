6576bd17ea123c489bbae74c0a262b913438de26
Gerry Weißbach <gerry.w@gammaproduction.de>
2020-04-15 15:26:02 +0200

    Completely change the API for running tasks.
    It is required to fork a subprocess otherwise we can not run two commands. A verification if the given command was added so that malicous code should be prevented. The commands are mostly strictly defined now.

--- src/com/inet/gradle/setup/dmg/preferences/Helper/main.m
+++ src/com/inet/gradle/setup/dmg/preferences/Helper/main.m
@@ -25,7 +25,7 @@
 
 #include "NSString+MD5.h"
 
-int execCommand(NSString *command, NSArray *arguments, int expectedStatus);
+int execCommand(int argc, const char *argv[] );
 BOOL checkCommand( const char * arg, NSString* expectedCommand );
 
 /* This helper tool is used to launch actions with elevated privileges.
@@ -47,55 +47,75 @@ int main(int argc, const char * argv[]) {
     NSURL *plist = [[NSBundle bundleForClass:[Service class]] URLForResource:@"service" withExtension:@"plist"];
     Service *service = [[Service alloc] initWithPlistURL:plist];
     NSString *servicePath = [service pathForService];
+    const char *servicePathC = [servicePath UTF8String];
+    const char *launchCtlC = [LAUNCHCTL_PATH UTF8String];
     
     if (checkCommand(argv[1], SERVICE_ACTION_REMOVE ) && [servicePath isEqualToMD5CString:argv[2]] ) {
 
-        execCommand(LAUNCHCTL_PATH, @[ @"unload", servicePath ], 4);
-        return execCommand(@"rm", @[ servicePath ], 0);
+        const char *unload[] = { launchCtlC, "unload", servicePathC, NULL };
+        execCommand( 3, unload );
+
+        const char *remove[] = { "rm", servicePathC, NULL };
+        return execCommand( 2, remove );
 
     } else if (checkCommand(argv[1], SERVICE_ACTION_INSTALL ) && [servicePath isEqualToMD5CString:argv[2]] ) {
 
         // there are cases that the service can not be restarted. Especially if it was killed.
-        NSString *source = [NSString stringWithUTF8String:[service.plist fileSystemRepresentation]];
-        int returnCode = execCommand(@"ln", @[ source, servicePath ], 4);
-        if ( returnCode != 4) {
-            fprintf(stderr, "Could not link the given service list file\n");
-            return 1;
-        }
-        
-        return execCommand(LAUNCHCTL_PATH, @[ @"load", servicePath ], 0);
+        const char *source = [[NSString stringWithUTF8String:[service.plist fileSystemRepresentation]] UTF8String];
+        const char *link[] = { "ln", source, servicePathC, NULL };
+        execCommand( 3, link );
+
+        const char *load[] = { launchCtlC, "load", servicePathC, NULL };
+        return execCommand( 3, load );
+
+    } else if ( checkCommand( argv[1], SERVICE_ACTION_UNINSTALL_SOFTWARE ) ) {
+           
+           int __block responseCode = 1;
+           [NSSearchPathForDirectoriesInDomains(NSPreferencePanesDirectory, NSAllDomainsMask, YES) enumerateObjectsUsingBlock:^(NSString *path, NSUInteger idx, BOOL *stop){
+               NSString *prefPane = [path stringByAppendingPathComponent:[[[NSBundle bundleForClass:[Service class]] bundlePath] lastPathComponent]];
+               if ( [prefPane isEqualToMD5CString:argv[2]] && [[NSFileManager defaultManager] fileExistsAtPath:prefPane] ) {
+                   const char *prefPaneC = [prefPane UTF8String];
+                   const char *remove[] = { "rm", prefPaneC };
+                   responseCode = execCommand( 2, remove );
+                   *stop = YES;
+               }
+           }];
+
+           return responseCode;
 
     } else if ( checkCommand( argv[1], SERVICE_ACTION_RUNAS ) ) {
 
         NSDictionary *starter = [service starterForHash:argv[2] ];
         if ( starter == nil ) {
             fprintf(stderr, "Unknown run as user command with hash: %s\n", argv[2]);
-            return 1;
+            return 5;
         }
 
         NSString *asUser = [Service userFor:starter];
+        const char *asUserC = [asUser UTF8String];
         if ( [@"root" isEqualToString:[asUser lowercaseString] ] ) {
             fprintf(stderr, "Cowardly refusing to run command hash `%s` as the _root_ user\n", argv[2]);
-            return 1;
+            return 6;
+        }
+
+        NSString *applicationRoot = [[[[service program] stringByDeletingPathExtension] stringByDeletingPathExtension] stringByDeletingPathExtension];
+        if ( [[applicationRoot pathComponents] count] < 2 || ![[NSFileManager defaultManager] fileExistsAtPath:applicationRoot] ) {
+            fprintf(stderr, "Something is wrong with the path determined as application root: %s\n", [applicationRoot UTF8String]);
+            return 7;
         }
         
-        return execCommand( @"sudo", @[ @"-u", asUser, @"/bin/bash", @"-c", [Service actionFor:starter] ], 4 );
-    } else if ( checkCommand( argv[1], SERVICE_ACTION_UNINSTALL_SOFTWARE ) ) {
+        if ( ![[[NSBundle bundleForClass:[Service class]] bundlePath] hasPrefix:applicationRoot] ) {
+            fprintf(stderr, "The application root '%s' is not a parent path of the current preferences bundle!\n", [applicationRoot UTF8String]);
+            return 8;
+        }
         
-        int __block responseCode = 1;
-        [NSSearchPathForDirectoriesInDomains(NSPreferencePanesDirectory, NSAllDomainsMask, YES) enumerateObjectsUsingBlock:^(NSString *path, NSUInteger idx, BOOL *stop){
+        const char *workingDir = [[NSString stringWithFormat:@"PWD=%@", applicationRoot] UTF8String];
+        const char *action = [[Service actionFor:starter] UTF8String];
+        const char *sudoing[] = { "sudo", "-b","-u", asUserC, "-i", workingDir, "/bin/bash", "-c", action, NULL };
+        return execCommand( 9, sudoing );
 
-            NSString *prefPane = [path stringByAppendingPathComponent:[[[NSBundle bundleForClass:[Service class]] bundlePath] lastPathComponent]];
-
-            if ( [prefPane isEqualToMD5CString:argv[2]] && [[NSFileManager defaultManager] fileExistsAtPath:prefPane] ) {
-                responseCode = execCommand(@"rm", @[ prefPane ], 4);
-                *stop = YES;
-            }
-        }];
-
-        return responseCode;
     } else {
-        fprintf(stderr, "Unknown action: %s\n", argv[1]);
+        fprintf(stderr, "Unknown action: `%s`\n", argv[1]);
         return 1;
     }
 }
@@ -103,16 +123,39 @@ int main(int argc, const char * argv[]) {
 /**
     Run the Command given with the parameters set
  */
-int execCommand(NSString *command, NSArray *arguments, int expectedStatus) {
+int execCommand(int argc, const char *argv[] ) {
 
-    char** args = [arguments getArray];
-    fprintf(stderr, "Running `%s %s`\n", [command UTF8String], [[arguments componentsJoinedByString:@" "] UTF8String]);
+    pid_t child_pid;
+    int child_status = 0;
+    
+    NSMutableArray * tmpary = [[NSMutableArray alloc] initWithCapacity: argc];
+    for (int i=0; i < argc; i++)
+    {
+        [tmpary addObject: [NSString stringWithCString: argv[i] encoding:NSASCIIStringEncoding]];
+    }
+    
+    DLog(@"Running command: `%@`", [tmpary componentsJoinedByString:@"` `"]);
 
-    execvp([command UTF8String], args );
-
-    [NSArray freeArray:args];
-    perror("execvp");
-    return expectedStatus;
+    child_pid = fork();
+    if(child_pid == 0) {
+        /* This is done by the child process. */
+        execvp(argv[0], (char* const*)&argv[0] );
+        /* If execvp returns, it must have failed. */
+        perror("execvp");
+        DLog( @"Command produced an error: %s", strerror(errno) );
+        exit(1); // we need to end this path of execution anyway! will be multiplied by 256;
+    }
+    else {
+       /* This is run by the parent.  Wait for the child
+          to terminate. */
+        pid_t tpid;
+        do {
+            tpid = wait(&child_status);
+        } while(tpid != child_pid);
+    }
+    
+    DLog( @"Command Result Status: %d\n", child_status);
+    return child_status;
 }
 
 /**
