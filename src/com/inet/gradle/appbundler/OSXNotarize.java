9aff241a86e1797b3ee530840d6177256da222bb
adrian <adrian-sw@aporter.org>
2020-04-29 01:53:12 -0400

    starting with java 11, the jdk no longer has a subdirectory "jre"
    you'll need an actual jre
    
    this fixes NullPointerException
    
    also remove unused import to class that no longer exists.

--- src/com/inet/gradle/appbundler/OSXNotarize.java
+++ src/com/inet/gradle/appbundler/OSXNotarize.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.gradle.api.internal.changedetection.state.IntegerValueSnapshot;
 import org.gradle.api.internal.file.FileResolver;
 
 import com.inet.gradle.appbundler.utils.xmlwise.Plist;
